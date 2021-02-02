a4a45c5244037bba19663123075430c91257276c
Emma Thierry <emmasbubby59@gmail.com>
2021-02-02 09:55:13 +0100

    Updated hashing

--- src/main/Communicator.java
+++ src/main/Communicator.java
@@ -6,6 +6,8 @@ import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.spec.InvalidKeySpecException;
 
@@ -29,7 +31,17 @@ public class Communicator {
 	}
 	
 	public static String hash(String data) throws NoSuchAlgorithmException, InvalidKeySpecException {
-		return data;
+		MessageDigest m = MessageDigest.getInstance("MD5");
+		m.reset();
+		m.update(data.getBytes());
+		byte[] digest = m.digest();
+		BigInteger bigInt = new BigInteger(1,digest);
+		String hashtext = bigInt.toString(16);
+		// Now we need to zero pad it if you actually want the full 32 chars.
+		while(hashtext.length() < 32 ){
+		  hashtext = "0"+hashtext;
+		}
+		return hashtext;
 	}
 	
 	public static String readJson(String path) throws IOException {
